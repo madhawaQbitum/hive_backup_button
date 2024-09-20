3e67baee821c88ea1a27761df32c42cfe4abe6b2
madhawaQbitum <madhawa@qbitum.com>
2024-09-20 10:15:19 +0530

    change dialog ui

--- lib/src/hive_backup_button.dart
+++ lib/src/hive_backup_button.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart' as material;
 class HiveBackupButton extends StatefulWidget {
   const HiveBackupButton({
     super.key,
@@ -23,34 +24,44 @@ Get.defaultDialog(
   middleText: "Do you want to Backup the App Data?",
   textConfirm: "Yes",
   textCancel: "No",
-  onConfirm: ()async {
-    const String backupDirectoryPath = '/storage/emulated/0/backup';
-    final Directory backupDirectory = Directory(backupDirectoryPath);
+  confirm: material.FilledButton(
+    onPressed: () async{
+      const String backupDirectoryPath = '/storage/emulated/0/backup';
+      final Directory backupDirectory = Directory(backupDirectoryPath);
 
 
-    if (!await backupDirectory.exists()) {
-    await backupDirectory.create(recursive: true);
-    }
+      if (!await backupDirectory.exists()) {
+      await backupDirectory.create(recursive: true);
+      }
 
 
-    for (Box box in widget.hiveBox) {
-    final String boxFilePath = '${box.path}';
-    final String boxBackupPath = '$backupDirectoryPath/${box.name}.hive';
+      for (Box box in widget.hiveBox) {
+      final String boxFilePath = '${box.path}';
+      final String boxBackupPath = '$backupDirectoryPath/${box.name}.hive';
 
 
-    final File originalBoxFile = File(boxFilePath);
-    if (await originalBoxFile.exists()) {
-    await originalBoxFile.copy(boxBackupPath);
-    }
-    }
+      final File originalBoxFile = File(boxFilePath);
+      if (await originalBoxFile.exists()) {
+      await originalBoxFile.copy(boxBackupPath);
+      }
+      }
 
-    ScaffoldMessenger.of(Get.context!).showSnackBar(
-    const SnackBar(content: Text('Backup completed successfully!')),
-    );
-  },
-  onCancel: () {
-    Get.back();
-  },
+      ScaffoldMessenger.of(Get.context!).showSnackBar(
+      const SnackBar(content: Text('Backup completed successfully!')),
+      );
+      Get.back();
+    },
+    child: const material.Text('Yes'),
+  ),
+  cancel: material.FilledButton(
+    onPressed: () {
+      Get.back();
+
+    },
+    child: const material.Text('No'),
+  ),
+  backgroundColor: Get.theme.colorScheme.surface,
+
 
 );
 
